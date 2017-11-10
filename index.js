1ca9c22472f2b2afb5d5cff3ca2bbe877e74e330
Alex Johnson <alex@gritdesign.co>
2017-11-10 20:28:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object)
+  var newObj = object
   return newObj
   delete newObj.key
   return newObj
